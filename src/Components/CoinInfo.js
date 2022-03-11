6316e77f590b3d22b38b84c413c199ea1a945f53
Siddhesh0002 <76722388+Siddhesh0002@users.noreply.github.com>
2022-03-11 16:37:36 +0530

    changes

--- src/Components/CoinInfo.js
+++ src/Components/CoinInfo.js
@@ -11,7 +11,7 @@ import {
 import SelectButton from "./SelectButton";
 import { chartDays } from "../Config/data";
 import { CryptoState } from "../CryptoContext";
-
+import Chart from 'chart.js/auto'
 const CoinInfo = ({ coin }) => {
   const [historicData, setHistoricData] = useState();
   const [days, setDays] = useState(1);
